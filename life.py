9342a290897a16e7c72ab805b4b19942bd8fc20d
Joel Gluth <jgluth@expediagroup.com>
2020-04-27 16:56:05 +0100

    Initial commit

---
+++ life.py
@@ -0,0 +1,79 @@
+
+
+
+class Arena:
+
+
+    def __init__(self):
+        self._aliveSet = set()
+
+        
+    def alive(self, x, y):
+        return (x, y) in self._aliveSet
+        
+
+    def setAlive(self, x, y):
+        self._aliveSet.add((x, y))
+
+    def setDead(self, x, y):
+        point = (x, y)
+        if point in self._aliveSet:
+            self._aliveSet.remove(point)
+
+    def countNeighbours(self, x, y):
+        count = 0
+        
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                if (not (i, j) == (0, 0)) and self.alive(x + i, y + j):
+                    count = count + 1
+
+        return count
+
+    
+    def survives(self, x, y):
+        
+        if self.alive(x, y):
+            if self.countNeighbours(x, y) in [2, 3]:
+                return True
+        else:
+            if self.countNeighbours(x, y) == 3:
+                return True
+        
+        return False
+
+
+    def next(self):
+        
+        interestingCells = set()
+        for (x, y) in self._aliveSet:
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    interestingCells.add((x + i, y + j))
+
+        nextAlive = set()
+        for (x, y) in interestingCells:
+            if self.survives(x, y):
+                nextAlive.add((x, y))
+
+        self._aliveSet = nextAlive
+
+    
+    def __eq__(self, other):
+        return isinstance(other, Arena) and self._aliveSet == other._aliveSet
+
+
+def emptyArena():
+    return Arena()
+
+
+def stringArena(alist):
+
+    a = Arena()
+    
+    for y in range(len(alist)):
+        for x in range(len(alist[y])):
+            if not alist[y][x] == '.':
+                a.setAlive(x, y)
+
+    return a
---
+++ life_test.py
@@ -0,0 +1,148 @@
+
+from life import *
+
+
+def emptyArena():
+    return Arena()
+
+
+def test_EmptyArena():
+    assert emptyArena() != None
+
+
+def test_EmptyArenaCellsNotAlive():
+    assert not emptyArena().alive(0, 0)
+
+
+def test_EmptyArenaCellsCanBeMadeAlive():
+    a = emptyArena()
+    a.setAlive(0, 0)
+    assert a.alive(0, 0)
+    assert not a.alive(0, 1)
+
+
+def test_EmptyArenaCellsCanBeMadeDead():
+    a = emptyArena()
+    a.setAlive(0, 0)
+    a.setDead(0, 0)
+    assert not a.alive(0, 0)
+
+
+def test_AlreadyDeadCellsCanBeMadeDead():
+    a = emptyArena()
+    a.setDead(0, 0)
+    assert not a.alive(0, 0)
+
+
+def stringArena(alist):
+
+    a = Arena()
+    
+    for y in range(len(alist)):
+        for x in range(len(alist[y])):
+            if not alist[y][x] == '.':
+                a.setAlive(x, y)
+
+    return a
+
+
+def threeSquareDiagonal():
+    return stringArena(['X..',
+                        '.X.',
+                        '..X'])
+
+def test_StringArena():
+    a = threeSquareDiagonal()
+    assert a.alive(0, 0)
+    assert a.alive(1, 1)
+    assert a.alive(2, 2)
+    assert not a.alive (0, 1)
+
+
+
+def test_Equality():
+    a = threeSquareDiagonal()
+    b = threeSquareDiagonal()
+
+    assert a == b
+
+
+def test_Inequality():
+    a = threeSquareDiagonal()
+
+    b = stringArena(['X..',
+                     '.X.',
+                     '...'])
+
+    assert not a == b
+
+
+def test_LiveCellWithFewerThanTwoLiveNeighboursDies():
+    a = threeSquareDiagonal()
+    assert not a.survives(0, 0)
+    assert not a.survives(2, 2)
+
+
+
+def test_CountNeighbours():
+
+    a = threeSquareDiagonal()
+    assert a.countNeighbours(0, 0) == 1
+    assert a.countNeighbours(1, 1) == 2
+    
+    
+def test_LiveCellWithTwoOrThreeLiveNeighboursSurvives():
+    a = stringArena(['X..',
+                     '.X.',
+                     '.XX'])
+    assert a.survives(2, 2), "two neighbours survives"
+    assert a.survives(1, 2), "three neighbours survives"
+
+
+def test_LiveCellWithMoreThanThreeLiveNeighboursDies():
+    a = stringArena(['X..',
+                     '.XX',
+                     '.XX'])
+    assert not a.survives(1, 1)
+
+
+def test_DeadCellWithExactlyThreeLiveNeighboursBecomesAlive():
+    a = stringArena(['X..',
+                     '.X.',
+                     '.XX'])
+    assert a.survives(2, 1)
+
+
+def block():
+    return stringArena(['....',
+                         '.XX.',
+                         '.XX.',
+                         '....'])
+
+def test_StillLifeBlock():
+    a = block()
+    a.next()
+    assert a == block()
+
+
+def vbar():
+    return stringArena(['.....',
+                        '..X..',
+                        '..X..',
+                        '..X..',
+                        '.....'])
+
+def hbar():
+    return stringArena(['.....',
+                        '.....',
+                        '.XXX.'
+                        '.....'
+                        '.....'])
+
+
+def test_OscillatorBlinker():
+    a = hbar()
+    a.next()
+    assert a == vbar()
+    a.next()
+    assert a == hbar()
